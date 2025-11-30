a64036108705e8459e133d5878553bb8fced1a8a
Caine Phung <cainephung2512@hotmail.com>
2025-11-29 22:32:34 -0500

    fixng close btn on mobile

--- src/components/WindowControls.jsx
+++ src/components/WindowControls.jsx
@@ -1,15 +1,18 @@
 import React from "react";
-import {useWindowStore} from "#store/window.js";
+import { useWindowStore } from "#store/window.js";
 
-export const WindowControls = ({target}) => {
-    // const {closeWindow, minimizeWindow, maximizeWindow} = useWindowStore();
-    const {closeWindow} = useWindowStore();
+export const WindowControls = ({ target }) => {
+  const { closeWindow } = useWindowStore();
 
-    return (
-        <div id="window-controls">
-            <div className="close" onClick={() => closeWindow(target)}/>
-            <div className="minimize disabled"/>
-            <div className="maximize disabled"/>
-        </div>
-    );
+  return (
+    <div id="window-controls">
+      <div className="close mobile-tap" onClick={() => closeWindow(target)}>
+        {/* Only helps on mobile */}
+        <span className="sr-only mobile-only">Close</span>
+      </div>
+
+      <div className="minimize disabled"></div>
+      <div className="maximize disabled"></div>
+    </div>
+  );
 };
